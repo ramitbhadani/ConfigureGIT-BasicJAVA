d0292c84a7f3012c9022178074235577ada96f37
shivanimathur476 <31319997+shivanimathur476@users.noreply.github.com>
2017-12-22 23:33:58 -0500

    String5

--- ConfigureGIT-JAVA/src/javaconcepts/strings.java
+++ ConfigureGIT-JAVA/src/javaconcepts/strings.java
@@ -4,7 +4,7 @@ public class strings {
 	
 	public static String   str1 = "Configure-GIT-with-Jenkins";
 	public static String   str2 = "JAVA String Methods"; 
-	
+	public static String   str3 = "JAVA String Methods"; 
 	public static void main(String[] args) {
 		String[] str1Split = str1.split("[-]");
 		String[] str2Split = str2.split("\\s");
@@ -19,7 +19,17 @@ public class strings {
 			System.out.println("Length of "  +a1 +    " is "    + a1.length());
 		//	System.out.println(str2Split[i]);
 		}
-		
+		String concatStrings = str1.concat(" " +str2);
+		if(concatStrings.contains("Java")) {
+		System.out.println("This will concat the Strings " +concatStrings );
+		}
+		else {
+			System.out.println("Else This will concat the Strings ==> " +concatStrings );
+		}
+		System.out.println("Starts With Function  " +str2.startsWith("Configure"));
+		System.out.println("Ends With Function   " +str2.endsWith("Methods"));
+		String type = str3.toUpperCase();
+		System.out.println("Ignore Case With Function   " +str2.equalsIgnoreCase(type));
 	}
 
 }
